1d7a13011114e382f6e5f077e657914cdfcb20f8
Marija Panić <mpanic432@gmail.com>
2024-07-12 16:47:27 +0200

    Add method for retrieving all available choices. Add test case

---
+++ RSPLS_Backend/Application.Tests/ChoiceService.Test.cs
@@ -0,0 +1,32 @@
+﻿using Core.Choice;
+using Core.Shared;
+
+namespace Application.Tests
+{
+    [TestFixture]
+    public class ChoiceServiceTest
+    {
+        private readonly ChoiceService service;
+        public ChoiceServiceTest()
+        {
+            service = new ChoiceService();
+        }
+        [Test]
+        public void GelAllAvailableChoices_ReturnsAllChoicesInLowerCase()
+        {
+            string[] expected = Enum
+                .GetNames(typeof(Choice))
+                .Select(name => name.ToLower())
+                .ToArray();
+
+            Result<string[]> result = service.GelAllAvailableChoices();
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(result.IsSuccess, Is.True);
+                Assert.That(result.Data, Is.EqualTo(expected));
+                Assert.That(result.Data.Count, Is.EqualTo(expected.Length));
+            });
+        }
+    }
+}
