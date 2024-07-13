c5e7bf57ff8ee42a630cf40f2203065081c72adf
Marija Panić <mpanic432@gmail.com>
2024-07-13 21:48:38 +0200

    Use function prop

--- rpsls_client/components/game/section/players/Choice/choiceGrid.tsx
+++ rpsls_client/components/game/section/players/Choice/choiceGrid.tsx
@@ -1,19 +1,19 @@
 import ChoiceOption from "./choiceOption";
-import styles from "./choiceGrid.module.css";
 import { Stack } from "@mui/material";
 import { ChoiceState } from "@/models/choiceState";
 
 interface Props {
-    choices: Array<ChoiceState>
+    choices: Array<ChoiceState>,
+    handleChoiceClick: (id: number, name: string) => {}
 }
 
 export default function ChoicesGrid(props: Props) {
-    const { choices } = props;
+    const { choices, handleChoiceClick } = props;
     return (
         <Stack direction="row" spacing={4}>
             {choices.map(choice =>
             (
-                <ChoiceOption id={choice.id} key={choice.id} name={choice.name}></ChoiceOption>
+                <ChoiceOption id={choice.id} key={choice.id} name={choice.name} handleChoiceClick={handleChoiceClick}></ChoiceOption>
                 )
             )}
         </Stack>
--- rpsls_client/components/game/section/players/userPlayer/userPlayer.tsx
+++ rpsls_client/components/game/section/players/userPlayer/userPlayer.tsx
@@ -5,16 +5,28 @@ import LoadingOptions from "../Choice/loadingOptions";
 import ChoicesGrid from "../Choice/choiceGrid";
 import { getPlayerChoices } from "@/agent/agent";
 import { UseGameContext } from "@/store/GameContext";
+import setDecisionUser from "@/actions/decisionUser";
 
 export function UserPlayer() {
-    const gameContext = UseGameContext()
+    const gameContext = UseGameContext();
+
+    async function onChoiceClick(id: number, name: string) {
+        const finalResult = await setDecisionUser({
+            decision: {
+                id,
+                name
+            }
+        });
+        gameContext.updateState(finalResult);
+    }
+    
     return (
         <PlayerContainer userChoice={`${gameContext.state.decision.user === '' ? 'Make ' : ''} your choice`}>
             <>
                 {gameContext.state.result.label === '' && gameContext.state.decision.user === '' &&
                     (
                         <Suspense fallback={<LoadingOptions></LoadingOptions>}>
-                            <ChoiceOptions />
+                            <ChoiceOptions handleChoiceClick={onChoiceClick}/>
                         </Suspense>
                     )}
                 {gameContext.state.decision.user !== '' && <p>{gameContext.state.decision.user}</p>}
@@ -23,8 +35,13 @@ export function UserPlayer() {
     );
 }
 
-async function ChoiceOptions() {
+interface Props {
+    handleChoiceClick: (id: number, name: string) => {}
+}
+
+async function ChoiceOptions(props: Props) {
     const response = await getPlayerChoices();
 
-    return <ChoicesGrid choices={response}></ChoicesGrid>
+    return <ChoicesGrid choices={response} handleChoiceClick={props.handleChoiceClick}></ChoicesGrid>
 }
+
