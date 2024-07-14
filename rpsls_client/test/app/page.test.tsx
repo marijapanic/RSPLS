2c1893625df1d7ef86b0fe3d2b90bf0820b04604
Marija Panić <mpanic432@gmail.com>
2024-07-14 23:16:16 +0200

    wip first test

---
+++ rpsls_client/test/app/page.test.tsx
@@ -0,0 +1,16 @@
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import Home from '../../app/page';
+
+test('renders the Home component with correct content', () => {
+  render(<Home />);
+
+  const headerElement = screen.getByText(/Welcome to the RPSLS game/i);
+  expect(headerElement).toBeInTheDocument();
+
+  const descriptionElement = screen.getByText(/Rock, Paper, Scissors, Lizard, Spock is a game of chance/i);
+  expect(descriptionElement).toBeInTheDocument();
+
+  const quoteElement = screen.getByText(/"Rock, Paper, Scissors, Lizard, Spock is a game of chance that expands the traditional game of Rock, Paper, Scissors./i);
+  expect(quoteElement).toBeInTheDocument();
+});
