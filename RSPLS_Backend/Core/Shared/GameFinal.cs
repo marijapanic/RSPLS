81ec0a371dec8b52e5df13a41d9c4e3d35a8dd56
Marija Panić <mpanic432@gmail.com>
2024-07-14 11:42:35 +0200

    Add logic

---
+++ RSPLS_Backend/Core/Shared/GameFinal.cs
@@ -0,0 +1,27 @@
+﻿using Core.Game;
+
+namespace Core.Shared
+{
+    public static class GameFinal
+    {
+        public static GameOutcome DetermineWinner(Choice.Choice userChoice, Choice.Choice computerChoice)
+        {
+            if (userChoice.Equals(computerChoice))
+            {
+                return new GameOutcome(GameResult.Tie.ToString(), userChoice, computerChoice);
+            }
+
+            GameResult outcome = (userChoice, computerChoice) switch
+            {
+                (Choice.Choice.Rock, Choice.Choice.Scissors) or (Choice.Choice.Rock, Choice.Choice.Lizard) => GameResult.Win,
+                (Choice.Choice.Paper, Choice.Choice.Rock) or (Choice.Choice.Paper, Choice.Choice.Spock) => GameResult.Win,
+                (Choice.Choice.Scissors, Choice.Choice.Paper) or (Choice.Choice.Scissors, Choice.Choice.Lizard) => GameResult.Win,
+                (Choice.Choice.Lizard, Choice.Choice.Spock) or (Choice.Choice.Lizard, Choice.Choice.Paper) => GameResult.Win,
+                (Choice.Choice.Spock, Choice.Choice.Scissors) or (Choice.Choice.Spock, Choice.Choice.Rock) => GameResult.Win,
+                _ => GameResult.Lose
+            };
+
+            return new GameOutcome(outcome.ToString(), userChoice, computerChoice);
+        }
+    }
+}
