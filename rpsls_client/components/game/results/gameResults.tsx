af136997229661008e50616830fec30c53a207c2
Marija Panić <mpanic432@gmail.com>
2024-07-14 10:57:12 +0200

    format

--- rpsls_client/components/game/results/gameResults.tsx
+++ rpsls_client/components/game/results/gameResults.tsx
@@ -2,19 +2,19 @@
 import { UseGameContext } from "@/store/GameContext";
 import { Stack } from "@mui/material";
 
-export default function GameResults()
-{
+export default function GameResults() {
     const gameContext = UseGameContext();
     return (
-        <Stack direction="row" maxWidth="small" 
-        sx= {{borderWidth: "1px",
-        borderRadius: "0.5em",
-        borderStyle: "solid",
-        justifyContent: "center",
-        borderColor: "#fff",
-        minHeight: "7.5em",
-        p: "1em"
-        }}>
+        <Stack direction="row" maxWidth="small"
+            sx={{
+                borderWidth: "1px",
+                borderRadius: "0.5em",
+                borderStyle: "solid",
+                justifyContent: "center",
+                borderColor: "#fff",
+                minHeight: "7.5em",
+                p: "1em"
+            }}>
             <Stack p="1em" display="flex" alignItems="center">
                 <h2>Computer</h2>
                 <p>{gameContext.state.games.computer}</p>
