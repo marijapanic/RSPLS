4d7157e41d18992ce1ad9dc338ed41c12445ff0b
Marija Panić <mpanic432@gmail.com>
2024-07-14 21:30:01 +0200

    docker support

--- rpsls_client/agent/agent.ts
+++ rpsls_client/agent/agent.ts
@@ -1,9 +1,11 @@
 import { ChoiceState, GameResult } from "@/models/choiceState";
 
+const DOMAIN = process.env.CHOICES_DOMAIN ? process.env.CHOICES_DOMAIN : process.env.NEXT_PUBLIC_CHOICES_DOMAIN;
+
 export async function getPlayerChoices(): Promise<Array<ChoiceState>> {
     try {
-        const optionsResponse = await fetch(`${process.env.NEXT_PUBLIC_CHOICES_DOMAIN}/api/choices`);
-
+        const optionsResponse = await fetch(`${DOMAIN}/api/choices`);
+        
         return await optionsResponse.json();
     } catch (error) {
         console.warn(error);
@@ -12,13 +14,13 @@ export async function getPlayerChoices(): Promise<Array<ChoiceState>> {
 }
 
 export async function getRandomChoice(): Promise<ChoiceState> {
-    const optionsResponse = await fetch(`${process.env.NEXT_PUBLIC_CHOICES_DOMAIN}/api/choice`);
+    const optionsResponse = await fetch(`${DOMAIN}/api/choice`);
 
     return await optionsResponse.json();
 }
 
 export async function getGameResult(userDecision: number): Promise<GameResult> {
-    const optionsResponse = await fetch(`${process.env.NEXT_PUBLIC_CHOICES_DOMAIN}/api/play`, {
+    const optionsResponse = await fetch(`${DOMAIN}/api/play`, {
         method: 'POST',
         body: JSON.stringify({
             player: userDecision
